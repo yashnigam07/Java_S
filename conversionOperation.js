5993e2da01e1f0ebab0201b613d81cbd9fa5485c
yashnigam07 <username.>
2024-06-27 20:31:43 +0530

    Updated co/OP file.

--- conversionOperation.js
+++ conversionOperation.js
@@ -10,7 +10,36 @@
 //string = True
 // null, undefined = False
 
- let a = undefined;
- let b = Boolean(a);
- console.log(b)
- console.log(typeof b)
+//  let a = undefined;
+//  let b = Boolean(a);
+//  console.log(b)
+//  console.log(typeof b)
+
+ // **************** Operations **************
+
+//  let value = 3;
+//  let negValue = -value
+//  console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1" + 2 + 2);
+// console.log(1+2+'2');
+// console.log((3+4)*5%3);
+
+let count = 100;
+y= count++; // Postfix - the increment operator increments and returns the value before incrementing.
+
+x = ++count; //Prefix -  the increment operator increments and returns the value after incrementing.
+console.log(count);
+console.log(x);
+console.log(y);
+
+
+
