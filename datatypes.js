e09c000274777ae8fb6ea7638881ca5a067c9dec
yashnigam07 <username.>
2024-06-27 21:19:28 +0530

    Update datatypes file

--- datatypes.js
+++ datatypes.js
@@ -14,4 +14,31 @@ object
 
 // console.log(typeof null);
 
+// Primitve - 7 types (String, boolean, number, null, undefined, symbol, BigInt)
+
+// Reference (Non primitive)
+
+//Array, Objects, functions
+
+// const score = 100;
+// const scoreValue = 100.4;
+// const isLoggedIn = false;
+const outsideTemp = null;
+// let userEmail;
+
+// const id = Symbol('123');
+// const anotherid = Symbol('123');
+// console.log(id===anotherid);
+
+// const bigNumber = 123456789123456789;
+// console.log(bigNumber);
+
+const heros = [1,2,3,4,5];
+let a={
+    name:"yash",
+    age:12
+};
+console.log(typeof heros); //object
+console.log(typeof a); //object
+console.log(typeof outsideTemp); //object
 
