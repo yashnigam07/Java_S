ea21f6bd223bbfe18c12f58f4219f36cb7d62c00
yashnigam07 <username.>
2024-06-27 20:56:59 +0530

    adding comparison file

---
+++ comparison.js
@@ -0,0 +1,30 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("1">1);
+// console.log("2"<2);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null >=0);
+
+// console.log(undefined==0);
+// console.log(undefined>2);
+// console.log(undefined<2);/
+
+// Avoid writing code in these format.
+
+
+// "===" user for strict comparison, it also compare tha data tyes of the variables.
+
+// console.log("2"===2);
+// console.log("2"===2);
+// console.log(null===3);
+
+
+
+
