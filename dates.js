6e912adb3282bcb78db18812dc2a8238bd41140d
yashnigam07 <username.>
2024-06-28 01:15:09 +0530

    Adding dates files

---
+++ dates.js
@@ -0,0 +1,12 @@
+// Dates
+
+let mydate = new Date();
+console.log(mydate);
+console.log(mydate.toDateString());
+console.log(mydate.toJSON());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.toString());
+
