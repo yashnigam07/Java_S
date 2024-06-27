aef8b2b039dad5fbbf0bbc4b106ee31419396900
yashnigam07 <username.>
2024-06-27 19:44:38 +0530

    Datatypes

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+// Data types
+/*
+number -> 2^35
+bigint
+string ->
+boolean -> true/false
+null -> standalone value
+undefined -> 
+symbol -> unique
+object 
+*/
+
+console.log(typeof null);
