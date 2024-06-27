bf41e8f7c49ecd6afdf98248710ff54bbc192a57
yashnigam07 <username.>
2024-06-28 00:39:31 +0530

    Addin math file

---
+++ math.js
@@ -0,0 +1,33 @@
+// const score = 400;
+// console.log(score);
+
+// const balance=new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const anothernumber=12.39;
+// console.log(anothernumber.toPrecision(4));
+// const a = 1000000;
+// console.log(a.toLocaleString('en-In')); //Give no in Indain no system.
+
+// *************** Matha **************
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));/
+
+// console.log(Math.min(1,2,3,4));
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
+
