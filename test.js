70ea2a5e5f96792741da84de6e0f42955602bcd0
yashnigam07 <username.>
2024-06-27 18:38:02 +0530

    Let and const

---
+++ test.js
@@ -0,0 +1,11 @@
+const accountId = 12
+let accountEmail = "hitesh@google.com"
+var accoutnPassword = "12345"
+let accoutCity;
+/* Prefer not use var because of issue in block scope and functional scope*/
+
+accountEmail = "asyg"
+accoutnPassword = '12'
+
+
+console.table([accountId, accountEmail, accoutnPassword, accoutCity])
