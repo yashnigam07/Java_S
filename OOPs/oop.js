d06c38033bf54a3956a5883e743d08741d9329ff
yashnigam07 <username.>
2024-07-20 21:23:22 +0530

    Modified oops file

--- OOPs/oop.js
+++ OOPs/oop.js
@@ -1,42 +1,59 @@
-// const user = {
-//     username:"Yash Nigam",
-//     loginCount: 4,
-//     signedIn: true,
-//     getdetails: function(){
-//         // return("Got user details from DB.")
-//         return(`Username:${this.username},This:${this}`);
+// // const user = {
+// //     username:"Yash Nigam",
+// //     loginCount: 4,
+// //     signedIn: true,
+// //     getdetails: function(){
+// //         // return("Got user details from DB.")
+// //         return(`Username:${this.username},This:${this}`);
 
+// //     }
+// // }
+// // console.log(user.loginCount);
+// // console.log(user.getdetails());
+// // console.log(this);
+
+// function User(username, loginCount, isLoggedIn) {
+//     this.username = username;
+//     this.loginCount = loginCount;
+//     this.isLoggedIn = isLoggedIn;
+//     this.greeting = function(){
+//         console.log(`Welcome ${this.username}`);
 //     }
 // }
-// console.log(user.loginCount);
-// console.log(user.getdetails());
-// console.log(this);
 
-function User(username, loginCount, isLoggedIn) {
+// const userone = new User("Yash", 12, true);
+// const usertwo = new User("Yash_2",13,true);
+// console.log(userone);
+// // console.log(usertwo);
+
+// function Car(make, model, year) {
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+//   }
+//   const auto = new Car('Honda', 'Accord', 1998);
+  
+//   console.log(auto instanceof Car);
+//   // Expected output: true
+  
+//   console.log(auto instanceof Object);
+//   // Expected output: true
+  
+function CreateUser(username, score) {
     this.username = username;
-    this.loginCount = loginCount;
-    this.isLoggedIn = isLoggedIn;
-    this.greeting = function(){
-        console.log(`Welcome ${this.username}`);
-    }
+    this.score = score;
 }
 
-const userone = new User("Yash", 12, true);
-const usertwo = new User("Yash_2",13,true);
-console.log(userone);
-// console.log(usertwo);
+CreateUser.prototype.increment = function() {
+    this.score++;
+}
 
-function Car(make, model, year) {
-    this.make = make;
-    this.model = model;
-    this.year = year;
-  }
-  const auto = new Car('Honda', 'Accord', 1998);
-  
-  console.log(auto instanceof Car);
-  // Expected output: true
-  
-  console.log(auto instanceof Object);
-  // Expected output: true
-  
+CreateUser.prototype.printMe = function() {
+    console.log(`score is ${this.score}`);
+}
+
+const chai = new CreateUser("chai", 25);
+const tea = new CreateUser("tea", 250);
+
+chai.printMe();
 
