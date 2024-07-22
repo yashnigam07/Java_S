82f3720e54ba12ff92bb24c46b3f7c14727f3a57
yashnigam07 <username.>
2024-07-22 20:59:37 +0530

    Modified bind

--- math.js
+++ math.js
@@ -22,12 +22,46 @@
 
 // console.log(Math.floor(Math.random()*10)+1);
 
-const min = 10;
-const max = 20;
+// const min = 10;
+// const max = 20;
 
-console.log(Math.floor(Math.random()*(max-min+1)+min));
+// console.log(Math.floor(Math.random()*(max-min+1)+min));
 
+// Object.getOwnPropertyDescriptor(Math)
 
+const descripter = Object.getOwnPropertyDescriptor(Math,"PI")
+console.log(Math.PI);
 
+// Creating an object with no prototype
+const mynewObject = Object.create(null);
 
+// Defining an object with some properties
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
 
+    orderchai: function(){
+        console.log("Mein chai nhi peeta.");
+    }
+}
+
+// Trying to get a non-existing property descriptor will return undefined
+console.log(Object.getOwnPropertyDescriptor(chai, "yash")); // undefined
+
+// Making the 'name' property non-writable and non-enumerable
+Object.defineProperty(chai, 'name', {
+    writable: false, // the property cannot be changed
+    enumerable: false // the property will not show up in for...in loops or Object.keys()
+});
+
+// Verify the changes
+console.log(Object.getOwnPropertyDescriptor(chai, "name")); // { value: 'ginger chai', writable: false, enumerable: false, configurable: false }
+console.log(Object.keys(chai)); // ["price", "isAvailable"]
+
+for (let [key, value] of Object.entries(chai)){
+    if (typeof value !=='function')
+    {
+        console.log(`${key}:${value}`);
+    }
+}
